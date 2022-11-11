61f86c85fb1e23cf38ee40c0e8d19b19840580ea
JSR <729326443@qq.com>
2022-11-11 16:11:47 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) { //敲psvm
         System.out.println("hello git!"); //先敲入"hello git!"，然后敲.sout，点左侧的绿色箭头，可以运行main函数
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
