9e977fd0f2f4b7a336c950325f6201d745bf9533
JSR <729326443@qq.com>
2022-11-11 16:01:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,7 @@
+package com.atguigu.git;
+
+public class GitTest {
+    public static void main(String[] args) { //敲psvm
+        System.out.println("hello git!"); //先敲入"hello git!"，然后敲.sout，点左侧的绿色箭头，可以运行main函数
+    }
+}
