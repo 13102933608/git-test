03cad5144f40e8e18f1d1b108e62d89c13b53a32
JSR <729326443@qq.com>
2022-11-11 16:06:25 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) { //敲psvm
         System.out.println("hello git!"); //先敲入"hello git!"，然后敲.sout，点左侧的绿色箭头，可以运行main函数
+        System.out.println("hello git2!");
     }
 }
